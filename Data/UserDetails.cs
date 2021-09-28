2058c40b32e5e00105ad6ae7576a2d5ac6d0bf9f
MaxMeulendijks <91459085+MaxMeulendijks@users.noreply.github.com>
2021-09-28 14:31:37 +0100

    User Input details

---
+++ Data/UserDetails.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TvLicenceProject.Data
+{
+    public class UserDetails
+    {
+        private string CourtDivision {get; set;}
+        private string AccountNumber {get; set;}
+        private string LicenceNumber {get; set;}
+        private string SurnameUser {get; set;}
+        private string PostcodeUser {get; set;}
+
+    }
