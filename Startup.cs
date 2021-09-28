b977cc77e145fd7a1b5630da23f6c9d7ede1e249
MaxMeulendijks <91459085+MaxMeulendijks@users.noreply.github.com>
2021-09-28 15:07:42 +0100

    add userinfo

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace TvLicenceProject
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-            services.AddSingleton<UserInfo>();
+            services.add<UserInfo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
