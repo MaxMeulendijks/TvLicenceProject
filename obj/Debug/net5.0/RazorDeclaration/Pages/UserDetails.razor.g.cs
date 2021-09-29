100165c377397429592b58429a23bcc963a1b835
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-09-29 15:56:20 +0100

    Input box css update

--- obj/Debug/net5.0/RazorDeclaration/Pages/UserDetails.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/UserDetails.razor.g.cs
@@ -98,7 +98,7 @@ using TvLicenceProject.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "J:\Documents\Code\TVLicenceProject\TvLicenceProject\Pages\UserDetails.razor"
+#line 43 "J:\Documents\Code\TVLicenceProject\TvLicenceProject\Pages\UserDetails.razor"
  
   UserInfo User = new UserInfo();
 
