5e67825a1919cf7e361f440205100cfcb3ca1f3a
MaxMeulendijks <91459085+MaxMeulendijks@users.noreply.github.com>
2021-09-29 16:32:32 +0100

    Adding in a database

---
+++ Data/UserDatabase.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.IList;
+
+namespace TvLicenceProject.Data
+{
+    public class UserDatabase
+    {
+            public int UserDatabase (UserInfo User)
+            {
+                ArrayList<UserInfo> DataEntries = new ArrayList<UserInfo>;
+
+                UserInfo Emily = new UserInfo;
+                Emily.CourtDivision = "C1";
+                Emily.UserFine = 150;
+
+                UserInfo Brian = new UserInfo;
+                Brian.CourtDivision = "C2";
+                Brian.UserFine = 100;
+
+                UserInfo Margret = new UserInfo;
+                Margret.CourtDivision = "C3";
+                Margret.UserFine = 0;
+
+                for(Userinfo ui in DataEntries)
+                {
+                    if(CompareCourtDivision(User, ui)==0)
+                    {
+                        return ui.UserFine;
+                    }
+                }
+
+                return -1;
+            }
+    }
+}
--- Data/UserInfo.cs
+++ Data/UserInfo.cs
@@ -10,6 +10,8 @@ namespace TvLicenceProject.Data
         public string UserSurname {get; set;}
         public string UserPostcode {get; set;}
 
+        public int UserFine {get; set;}
+
         public string CardNumber {get; set;}
         public string ExpiryMonth {get; set;}
         public string ExpiryYear {get; set;}
