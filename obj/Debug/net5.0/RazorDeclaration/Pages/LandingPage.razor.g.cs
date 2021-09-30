5677949b50d9254c5fde5d595395cf99bd10dcad
JoshuaMcroberts <josh.mcr@hotmail.co.uk>
2021-09-30 08:48:29 +0100

    Canecl Payment linked to landing page

--- obj/Debug/net5.0/RazorDeclaration/Pages/LandingPage.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/LandingPage.razor.g.cs
@@ -82,7 +82,7 @@ using TvLicenceProject.Shared;
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/landingpage")]
+    [Microsoft.AspNetCore.Components.RouteAttribute("/")]
     public partial class LandingPage : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
