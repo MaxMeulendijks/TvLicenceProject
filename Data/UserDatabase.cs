c593b8b0f98142e3e5d058574f02cda7538be634
MaxMeulendijks <91459085+MaxMeulendijks@users.noreply.github.com>
2021-09-30 12:21:43 +0100

    Try

--- Data/UserDatabase.cs
+++ Data/UserDatabase.cs
@@ -7,25 +7,49 @@ namespace TvLicenceProject.Data
 {
     public class UserDatabase
     {
-            public int CheckPresence (UserInfo User)
-            {
-                List<UserInfo> DataEntries = new List<UserInfo>();
+        List<UserInfo> DataEntries = new List<UserInfo>();
 
                 UserInfo Emily = new UserInfo();
                 Emily.CourtDivision = "C1";
+                Emily.UserSurname = "Bloggs";
+                Emily.LicenceNumber = "12345678";
                 Emily.UserFine = 150;
                 DataEntries.Add(Emily);
 
                 UserInfo Brian = new UserInfo();
                 Brian.CourtDivision = "C2";
+                Brian.UserSurname = "Bloggs";
+                Brian.LicenceNumber = "12345678";
                 Brian.UserFine = 100;
                 DataEntries.Add(Brian);
 
                 UserInfo Margret = new UserInfo();
                 Margret.CourtDivision = "C3";
+                Margret.UserSurname = "Bloggs";
+                Margret.LicenceNumber = "12345678";
                 Margret.UserFine = 0;
                 DataEntries.Add(Margret);
 
+            public bool CheckPresence (UserInfo User)
+            {
+                
+                CompareCourtDivision compareCourt = new CompareCourtDivision();
+
+                foreach(UserInfo ui in DataEntries)
+                {
+                    if(compareCourt.Equals(User, ui)==true)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+
+             public int FineReturn (UserInfo User)
+            {
+                
                 CompareCourtDivision compareCourt = new CompareCourtDivision();
 
                 foreach(UserInfo ui in DataEntries)
@@ -37,6 +61,22 @@ namespace TvLicenceProject.Data
                 }
 
                 return -1;
+            }
+
+
+            public int UserReturn (UserInfo User)
+            {
+                
+                CompareCourtDivision compareCourt = new CompareCourtDivision();
+
+                foreach(UserInfo ui in DataEntries)
+                {
+                    if(compareCourt.Equals(User, ui)==true)
+                    {
+                        return ui;
+                    }
+                }
+
             }
     }
 
